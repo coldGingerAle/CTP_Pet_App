cc47790c49e9522782b0aa11a1c950eb5ca1ff3e
KevinCybura <KevinCybura@gmail.com>
2017-10-09 10:55:13 -0400

    WebPack: Setup WebPack npm run build- will see if app compiles npm run dev-server- will run the app on localhost:8080
    Babel:
    setup Babel
    Babel will compile react code into older versions of javascript

---
+++ views/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PetApp from './components/PetApp';
+
+ReactDOM.render(<PetApp/>,document.getElementById('app'));
+
---
+++ views/components/PetApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class PetApp extends React.Component{
+
+  render(){
+    return (
+      <div>
+        <h1>Pet Rescue</h1>
+      </div>
+    )
+  }
+};
+
