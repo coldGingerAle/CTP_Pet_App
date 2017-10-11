fd0b933dd1b3c43b8fc0533cec0b8288dd842ad6
KevinCybura <KevinCybura@gmail.com>
2017-10-11 19:29:18 -0400

    small fixes

--- views/components/PetApp.js
+++ views/components/PetApp.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Header from 'header';
 export default class PetApp extends React.Component{
 
   render(){
     return (
       <div>
         <h1>Pet Rescue</h1>
+        <Header/>
+        <Nav/>
       </div>
     )
   }
---
+++ views/components/header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+export default class Header extends React.Component {
+  render (){
+    return 
+  }
+}
