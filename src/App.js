384880c925ca74bacd04c76bb42b8ca66b76189b
KevinCybura <KevinCybura@gmail.com>
2017-10-22 22:01:49 -0400

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch , Link} from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
-     <div>Working</div>
-    );
+      <div>
+        <button><Link to="/login">Login</Link></button>
+      </div>
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from './Component/Login';
+import { BrowserRouter, Route, Switch , Link} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const notFound = () => {
+  <div>404!</div>;
+};
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact={true} />
+      <Route path="/login" component={Login} />
+      <Route component={notFound}/>
+    </Switch>
+  </BrowserRouter>
+);
+ReactDOM.render(routes, document.getElementById('app'));
